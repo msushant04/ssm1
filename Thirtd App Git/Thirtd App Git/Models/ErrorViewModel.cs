6f284af74b2dde7b9f0be4020ced9634d6687462
Sushant Mahadik <sushant.mahadik@tadigital.com>
2021-04-23 10:40:00 +0530

    First Commit

---
+++ Thirtd App Git/Thirtd App Git/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Thirtd_App_Git.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
